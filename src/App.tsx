569ea01745d46fcd5a9e15e992ec794efe3a87b3
Leo Chan <chanwailok1994@gmail.com>
2020-06-28 18:00:44 +0800

    feat: base UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+
+import "./App.css";
+import AccountGenerator from "./components/AccountGenerator";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="light" expand="lg" sticky="top">
+        <Navbar.Brand href="">Bitcoin account generator</Navbar.Brand>
+      </Navbar>
+      <div>
+        <AccountGenerator></AccountGenerator>
+      </div>
     </div>
   );
 }
