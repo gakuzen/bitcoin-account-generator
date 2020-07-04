94d4564932d80d16709caf6812383e29cde2d413
Leo Chan <chanwailok1994@gmail.com>
2020-07-04 12:32:39 +0800

    feat: single sig account

--- src/components/SingleSigAccount.tsx
+++ src/components/SingleSigAccount.tsx
@@ -4,10 +4,45 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 
-import { generateMnemonic } from "../utils/bitcoin";
+import {
+  generateMnemonic,
+  AddressType,
+  defaultPathBIP49,
+  defaultPathBIP84,
+  generateAccount,
+} from "../utils/bitcoin";
 
 const SingleSigAccount = (): JSX.Element => {
-  const [mnemonic, setMnemonic] = useState<string>();
+  const [addressType, setAddressType] = useState<AddressType>(
+    AddressType.SegWit
+  );
+  const [mnemonic, setMnemonic] = useState<string>("");
+  const [path, setPath] = useState<string>("");
+  const [pathPlaceholder, setPathPlaceholder] = useState<string>(
+    defaultPathBIP49
+  );
+  const [address, setAddress] = useState<string>("");
+  const [publicKey, setPublicKey] = useState<string>("");
+  const [privateKey, setPrivateKey] = useState<string>("");
+
+  const changeAddressType = (addressType: AddressType) => {
+    switch (addressType) {
+      case AddressType.SegWit: {
+        setAddressType(AddressType.SegWit);
+        setPathPlaceholder(defaultPathBIP49);
+        setPath("");
+        break;
+      }
+      case AddressType.NativeSegWit: {
+        setAddressType(AddressType.NativeSegWit);
+        setPathPlaceholder(defaultPathBIP84);
+        setPath("");
+        break;
+      }
+      default: {
+      }
+    }
+  };
 
   return (
     <Card className="accountCard">
@@ -17,12 +52,37 @@ const SingleSigAccount = (): JSX.Element => {
       <Card.Body>
         <div>
           <Form>
+            <Form.Group controlId="formSegWitType">
+              <Form.Check
+                inline
+                label="SegWit"
+                type="radio"
+                name="formSegWitTypes"
+                id="formSegWitTypes1"
+                checked={addressType === AddressType.SegWit}
+                onChange={() => {
+                  changeAddressType(AddressType.SegWit);
+                }}
+              />
+              <Form.Check
+                inline
+                label="Native SegWit"
+                type="radio"
+                name="formSegWitTypes"
+                id="formSegWitTypes2"
+                checked={addressType === AddressType.NativeSegWit}
+                onChange={() => {
+                  changeAddressType(AddressType.NativeSegWit);
+                }}
+              />
+            </Form.Group>
+
             <Form.Group controlId="formSeed">
               <Form.Label>Seed Phrase</Form.Label>
               <InputGroup>
                 <Form.Control
                   as="textarea"
-                  rows={2}
+                  rows={3}
                   placeholder="Enter a seed phrase"
                   value={mnemonic}
                   onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
@@ -47,15 +107,71 @@ const SingleSigAccount = (): JSX.Element => {
 
             <Form.Group controlId="formPath">
               <Form.Label>Derivation Path</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter path, eg. m/0'/0/0"
-              />
+              <InputGroup>
+                <Form.Control
+                  type="text"
+                  placeholder={`Enter path, eg. ${pathPlaceholder}`}
+                  value={path}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setPath(event.target.value);
+                  }}
+                />
+                <InputGroup.Append>
+                  <Button
+                    variant="primary"
+                    onClick={(): void => {
+                      setPath(pathPlaceholder);
+                    }}
+                  >
+                    Default
+                  </Button>
+                </InputGroup.Append>
+              </InputGroup>
             </Form.Group>
 
-            <Button variant="primary" type="button">
-              Generate
-            </Button>
+            <Form.Group>
+              <Button
+                variant="primary"
+                type="button"
+                onClick={() => {
+                  const { account, node } =
+                    generateAccount(addressType, mnemonic, path) || {};
+                  if (account) {
+                    const { address } = account;
+                    setAddress(address || "");
+                  }
+
+                  if (node) {
+                    setPublicKey(node.publicKey.toString("hex"));
+                    setPrivateKey(node.toWIF());
+                  }
+                }}
+              >
+                Generate
+              </Button>
+            </Form.Group>
+            {address && (
+              <div>
+                <Form.Group controlId="formAddress">
+                  <Form.Label>Address</Form.Label>
+                  <InputGroup>
+                    <Form.Control type="text" value={address} readOnly />
+                  </InputGroup>
+                </Form.Group>
+                <Form.Group controlId="formPublicKey">
+                  <Form.Label>Public Key</Form.Label>
+                  <InputGroup>
+                    <Form.Control type="text" value={publicKey} readOnly />
+                  </InputGroup>
+                </Form.Group>
+                <Form.Group controlId="formPrivateKey">
+                  <Form.Label>Private Key</Form.Label>
+                  <InputGroup>
+                    <Form.Control type="text" value={privateKey} readOnly />
+                  </InputGroup>
+                </Form.Group>
+              </div>
+            )}
           </Form>
         </div>
       </Card.Body>
